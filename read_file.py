2b71cb940c66f3e123dff93235d29057742f2535
GasinAn <Gasin185@163.com>
2023-11-02 17:40:53 +0800

    add read_txt

--- read_file.py
+++ read_file.py
@@ -1,2 +1,4 @@
-def read_file(file_name):
-    pass
+import pandas as pd
+
+def read_txt(*args, **kwargs):
+    return pd.read_fwf(*args, **kwargs)
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import requests
 import gzip
 
+from read_file import *
+
 GAIADR3_SAM_URL = "https://cdsarc.cds.unistra.fr/ftp/I/355/gaiadr3.sam.gz"
 
 def download_gaiadr3_sam():
@@ -8,13 +10,18 @@ def download_gaiadr3_sam():
     r.raise_for_status()
     with open("gaiadr3.sam.gz", "wb") as gaiadr3_sam_gz:
         gaiadr3_sam_gz.write(r.content)
-    with gzip.open("gaiadr3.sam.gz", "rb") as gaiadr3_sam_gz:
-        content = gaiadr3_sam_gz.read()
-    with open("gaiadr3.sam", "wb") as gaiadr3_sam:
-        gaiadr3_sam.write(content)
 
 def test_read_txt():
-    download_gaiadr3_sam()
+    #download_gaiadr3_sam()
+    dataframe = read_txt(
+        "gaiadr3.sam.gz",
+        compression="gzip",
+        header=None,
+        names=["DR3Name", "RAdeg", "DEdeg"],
+        colspecs=[(0, 28), (29, 44), (45, 60)],
+    )
+    print(dataframe.dtypes)
+    print(dataframe)
 
 def test():
     test_read_txt()
